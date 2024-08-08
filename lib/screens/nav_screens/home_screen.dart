609a06aa917bea4f2d327f46249997d2ba3d9c21
Gokulashik M <goku22014.cd@rmkec.ac.in>
2024-08-08 18:03:30 +0530

    dumb

--- lib/screens/nav_screens/home_screen.dart
+++ lib/screens/nav_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/others/app_color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,9 +8,37 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("HomeScreen"),);
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          // Menu
+          Container(
+            margin: EdgeInsets.only(left: 12, right: 12, top: 12),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.menu),
+                Container(
+                  height: 54,
+                  width: 54,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: AppColor.grey),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    ));
   }
 }
